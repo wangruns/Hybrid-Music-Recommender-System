09e9e623f66d8d2ddc4362b2f30d219cc1937df5
wangruns <1344886192@qq.com>
2018-05-28 19:45:58 +0800

    fix a bug for calculating userd-based similarity

--- trackstacking/src/main/java/top/wangruns/trackstacking/algorithm/Similarity.java
+++ trackstacking/src/main/java/top/wangruns/trackstacking/algorithm/Similarity.java
@@ -2,14 +2,19 @@ package top.wangruns.trackstacking.algorithm;
 
 public class Similarity {
 
-	public static float calculateSimilarity(float[] fs, float[] fs2) {
+	public static float calculateSimilarity(float[] curRating, float[] otherRating) {
 		// TODO Auto-generated method stub
 		float similarity=0f;
-		int len=fs.length;
+		int len=curRating.length;
+		int cnt=0;
 		for(int i=0;i<len;i++) {
-			similarity+=Math.pow(fs[i]-fs2[i], 2);
+			//根据被当前用户“评分”了的项目来计算相似度，由于浮点数不能==比较，这里用0.01f表示0.0f
+			if(curRating[i]>0.01f) {
+				similarity+=Math.pow(curRating[i]-otherRating[i], 2);
+				cnt++;
+			}
 		}
-		similarity/=len;
+		similarity/=(cnt>0?cnt:1);
 		
 		return similarity;
 	}
